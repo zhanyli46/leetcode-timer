1114d21f03d3c5a20b50c627f0326974afbdfa44
Zhanyang Li <zhanyli46@gmail.com>
2019-03-05 21:33:22 -0800

    finished popup page, modified storage

--- background.js
+++ background.js
@@ -1,8 +1,8 @@
 chrome.runtime.onInstalled.addListener(function() {
   chrome.storage.sync.set({
-    easy: {hr: 0, min: 15, sec: 0},
-    medium: {hr: 0, min: 30, sec: 0},
-    hard: {hr: 0, min: 45, sec: 0}
+    easy: {hr: '00', min: '15', sec: '00'},
+    medium: {hr: '00', min: '30', sec: '00'},
+    hard: {hr: '00', min: '45', sec: '00'}
   });
   chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
     chrome.declarativeContent.onPageChanged.addRules([{
--- popup.js
+++ popup.js
@@ -1,18 +1,28 @@
-chrome.storage.sync.get(['easy', 'medium', 'hard'], function(result) {
-  var diff = ['easy', 'medium', 'hard'];
-  var seg = ['hr', 'min', 'sec'];
-  for (var d of diff) {
-    for (var s of seg) {
-      document.getElementById(`time_${d}_${s}`).innerText = result[d][s].toString().padStart(2, '0');
-    }
-  }
-});
+/* populate time setter */
+loadDefaultTime();
 
+/* bind event handlers */
 var segments = document.getElementsByClassName('time_set');
 for (var seg of segments) {
   seg.onmousewheel = segmentOnScroll;
 }
+document.getElementById('btn_accept').onclick = acceptOnClick;
+document.getElementById('btn_cancel').onclick = cancelOnClick;
 
+/* helper functions */
+function loadDefaultTime() {
+  chrome.storage.sync.get(['easy', 'medium', 'hard'], function(result) {
+    var diff = ['easy', 'medium', 'hard'];
+    var seg = ['hr', 'min', 'sec'];
+    for (var d of diff) {
+      for (var s of seg) {
+        document.getElementById(`time_${d}_${s}`).innerText = result[d][s];
+      }
+    }
+  });
+}
+
+/* event handlers */
 function segmentOnScroll(e) {
   var dlt = e.deltaY < 0 ? 1 : -1;
   var lobound = 0;
@@ -25,5 +35,18 @@ function segmentOnScroll(e) {
 }
 
 function acceptOnClick() {
-  // todo
+  var diff = ['easy', 'medium', 'hard'];
+  var seg = ['hr', 'min', 'sec'];
+  var data = {};
+  for (var d of diff) {
+    data[d] = {};
+    for (var s of seg) {
+      data[d][s] = document.getElementById(`time_${d}_${s}`).innerText;
+    }
+  }
+  chrome.storage.sync.set(data);
+}
+
+function cancelOnClick() {
+  loadDefaultTime();
 }
